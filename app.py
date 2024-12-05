3c45a1723d97f18f45be318ff46fb318d5c6d971
HonraVT <111161607+HonraVT@users.noreply.github.com>
2024-12-05 12:39:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from threading import Thread, Event
 from flask import Flask, request, render_template, jsonify
 
 from config import Config
-from kiss_xenforo import run, set_position, load_db
+from kiss_xenforo import main, set_position, load_db
 
 conf = Config()
 
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 data = {
     "running": False,
+    "thread_id": ""
 }
 
 stop_event = Event()
@@ -21,13 +22,12 @@ thread = None
 def run_thread():
     while not stop_event.is_set():
         try:
-            run()
+            main(data["thread_id"])
         except Exception as e:
             print(f"Erro durante a execução: {e}")
-            print(f"Reiniciando tarefa em {conf.ERROR_DELAY} segundos...")
+            # print(f"Reiniciando tarefa em {conf.ERROR_DELAY} segundos...")
             time.sleep(conf.ERROR_DELAY)
         else:
-            # print(f"Tarefa finalizada com sucesso. Reiniciando em {conf.DELAY} segundos...")
             time.sleep(conf.DELAY)
         finally:
             if stop_event.is_set():
@@ -41,7 +41,6 @@ def rank():
         try:
             new_position = int(request.form.get('position', 500))
             set_position(new_position)
-            return jsonify({"status": "success", "position": new_position}), 200
         except ValueError:
             return jsonify({"status": "error", "message": "Invalid position"}), 400
 
@@ -53,21 +52,31 @@ def configure():
     global thread
 
     message = "Aguardando ação."
+
     if request.method == 'POST':
         action = request.form.get('action')
+        new_thread_id = request.form.get('thread_id')
 
-        if action == 'start' and not data["running"]:
+        if action == 'update_id':
+            if new_thread_id:
+                data["thread_id"] = new_thread_id
+                message = f"ID atualizado para {new_thread_id}."
+
+        elif action == 'start' and not data["running"]:
             stop_event.clear()
             thread = Thread(target=run_thread, daemon=True)
             thread.start()
             data["running"] = True
             message = "Processo iniciado."
+            print(message)
+
         elif action == 'stop' and data["running"]:
             stop_event.set()
             if thread:
                 thread.join()
             data["running"] = False
             message = "Processo parado."
+
         elif action == 'restart':
             stop_event.set()
             if thread:
@@ -77,12 +86,16 @@ def configure():
             thread.start()
             data["running"] = True
             message = "Processo reiniciado."
-        else:
-            message = "Ação inválida ou processo já no estado solicitado."
+            print(message)
 
-    return render_template('configure.html', message=message, running=data["running"])
+    return render_template(
+        'configure.html',
+        message=message,
+        running=data["running"],
+        thread_id=data["thread_id"]
+    )
 
 
 if __name__ == '__main__':
-    # app.run(host='127.0.0.1', port=8000)
-    app.run(host="0.0.0.0", port=80)
+    app.run(host='127.0.0.1', port=8000)
+    # app.run(host='0.0.0.0', port=80)
